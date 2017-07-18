4a980b891410bf51d0961a3cd2f9467531145f52
Musab36 <musabhd@gmail.com>
2017-07-18 14:54:22 +0300

    api

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,10 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=[ctzsLu7FZoaMMoWxkwln8ai25Rf3spjc7RqgqD5V]&zip=' + params.zip;
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
       return responseJSON.results;
     });
